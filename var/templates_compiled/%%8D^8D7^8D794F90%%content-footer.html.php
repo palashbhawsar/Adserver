d123dfb758ea1c0a4abc40d21dc43bd38da60325
yingale <yogeshingale@Yogeshs-MacBook-Pro.local>
2017-07-19 13:53:13 +0530

    Done

---
+++ var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2017-07-19 15:22:43
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
